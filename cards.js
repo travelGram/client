e3653e268779a0022115526850b37d0bdb03c164
Odelia <odeliasusanto@gmail.com>
2018-08-16 14:20:06 +0700

    added fb share button

--- cards.js
+++ cards.js
@@ -1,22 +1,24 @@
 Vue.component('cards-pics', {
     template: `
     <div class="container">
-            <div class="row">
-                <div class="card" style="width: 18rem;" v-for="photo in pictures">
-                    <img v-bind:src="photo.imageUrl" class="card-img-top" >
-                    <div class="card-body">
-                        <div class="caption">
-                            <p>{{photo.caption}}</p>
-                            </div>
-                        <div class="card-footer">
-                            <div class="twitter-logo">
-                            <img src="twitter-logo.png" style="width:30px; height:30px;">
-                            </div>
-                        </div>
+    <div class="row">
+        <div class="card" style="width: 18rem;" v-for="photo in pictures">
+        <img v-bind:src="photo.imageUrl" :key="photo._id" class="card-img-top" href="singlecard.html">
+            <div class="card-body">
+                <div class="caption">
+                    <p>{{photo.caption}}</p>
+                </div>
+                <div class="card-footer">
+                    <div class="fb-share-button" :data-href="photo.imageUrl" data-layout="button" data-size="large"
+                        data-mobile-iframe="true">
+                        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
+                            class="fb-xfbml-parse-ignore">Share</a>
                     </div>
                 </div>
             </div>
         </div>
-    `, 
-    props:['pictures'],
+    </div>
+</div>
+    `,
+    props: ['pictures'],
 })
